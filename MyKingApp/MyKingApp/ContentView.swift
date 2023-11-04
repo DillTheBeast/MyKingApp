e5c36949a697b1fd9f12078351d28480ab6a2e7b
DillTheBeast <dillonmaltese@gmail.com>
2023-11-04 14:16:38 -0400

    asd

---
+++ MyKingApp/MyKingApp/ContentView.swift
@@ -0,0 +1,24 @@
+import SwiftUI
+import WebKit
+
+struct WebView: UIViewRepresentable {
+    let urlString: String
+    
+    func makeUIView(context: Context) -> WKWebView {
+        let webView = WKWebView()
+        return webView
+    }
+    
+    func updateUIView(_ uiView: WKWebView, context: Context) {
+        if let url = URL(string: urlString) {
+            let request = URLRequest(url: url)
+            uiView.load(request)
+        }
+    }
+}
+
+struct ContentView: View {
+    var body: some View {
+        WebView(urlString: "https://kingschoolct.myschoolapp.com/app/student#login")
+    }
+}
---
+++ MyKingApp/MyKingApp/MyKingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyKingAppApp.swift
+//  MyKingApp
+//
+//  Created by Dillon Maltese on 11/3/23.
+//
+
+import SwiftUI
+
+@main
+struct MyKingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
